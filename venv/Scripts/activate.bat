a20b3ffc68af5c5374d355d3a381ab0f9bb972cd
JoshuaMooneyham <jmooneyham58@gmail.com>
2024-05-15 19:35:47 -0500

    test

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\CaptSassyPant\Documents\VSCode\Unit 6\Fotispy\venv
+set VIRTUAL_ENV=C:\Users\whatt\OneDrive\Documents\VSCode\Projects\Unit 6\fotispy2\Fotispy\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
